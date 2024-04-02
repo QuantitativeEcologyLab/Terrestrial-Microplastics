b9c1b6fca6409cc5dde0cf67691cfe087a679e29
MILLS <lauren.mills@ubc.ca>
2024-04-02 15:26:13 -0700

    updated script b/c overhauled github repo

---
+++ Scripts/5_MP_predictions.R
@@ -0,0 +1,278 @@
+setwd("C:/Users/lmills96/OneDrive - UBC/MSc Thesis Info/Global Analysis/Global MP Distribution/Figures")
+
+# Load required packages
+
+library(mgcv)
+library(dplyr)
+library(gridExtra)
+library(khroma)
+
+# Load colourblind friendly colour palette
+bright <- color("bright")
+plot_scheme(bright(7), colours = TRUE, names = TRUE, size = 0.9)
+
+
+#--------------------------------------------------------------------------
+# Predicted trends in terrestrial microplastics   
+#--------------------------------------------------------------------------   
+
+# Make "Predictions" column in dataframe
+MPdf$Predictions <- predict(model, data = MPdf, type = "response", se = FALSE)
+
+
+
+# Depth Predictions -------------------------------------------------------
+
+# Make a new dataframe
+newdf_depth <- data.frame(Max_Depth_cm = seq(3, 250, by = 1),
+                          HFI = 1,
+                          Elevation_km = 0,
+                          Study = 'new study')
+
+prediction_depth <- newdf_depth
+# mu here is the same thing as saying "predictions/projections" -> we are using 
+# pred$mu to make a new column in the df with the title of mu
+newdata = newdf_depth,
+prediction_depth$mu <- predict(model, 
+                               se.fit = FALSE,
+                               type = 'response')
+# Note: the warning message here is indicating that there are new categories in 
+# the 'study' variable that were not present in the initial MPdf
+
+
+# Plot the predicted depth
+# Note: these predictions are only accounting for depth under the assumption 
+# that HFI is at 1 and elevation is at 0. Therefore, variance is unexplained 
+# b/c some of the depth / elevation is different within the plot  
+ggplot() + 
+  geom_point(aes(Max_Depth_cm, Items_kg), MPdf) +
+  geom_line(aes(Max_Depth_cm, mu), prediction_depth, col = 'red', lwd = 1) +
+  scale_y_log10() #good to have the log10 scale here 
+
+
+
+# Create credible intervals 
+# Note: we need type = "link" b/c we want to estimate credible intervals on the 
+# link scale - we can't do it otherwise. Also, mgcv is inherently Bayesian, so 
+# credible intervals are what we are looking at here (vs. confidence intervals)
+# Also, for non-normal distributions and non-identity link functions, se.fit = T
+# yields standard errors of the prediction, i.e., a measure of uncertainty for 
+# the predicted value. This prediction, by one of the Central Value Theorems, 
+# can be assumed to be normally distributed at the link scale, and hence its 
+# standard error can be given as the standard deviation of a normal distribution.
+fit_depth <- predict(model, newdata = prediction_depth, se.fit = TRUE,
+                               type = 'link') 
+
+#exp() is the exponential function, and in this context, it's used to reverse 
+#the log transformation that might have been applied during the modeling process
+mu_depth <- exp(fit_depth$fit)
+lower_95_depth <- exp(fit_depth$fit - 1.96 * fit_depth$se.fit) 
+upper_95_depth <- exp(fit_depth$fit + 1.96 * fit_depth$se.fit)
+
+# Combine dataframes
+prediction_depth_ci <- data.frame(prediction_depth,
+                                  fit_depth,
+                                  mu = mu_depth,
+                                  lower_95 = lower_95_depth,
+                                  upper_95 = upper_95_depth)
+
+# could also use dplyer() 
+
+
+# Plot the depth prediction
+pred_depth <-
+  ggplot() +
+  geom_point(aes(Max_Depth_cm, Items_kg), MPdf, col = "#924900") +
+  geom_ribbon(aes(Max_Depth_cm, ymin = lower_95, ymax = upper_95), prediction_depth_ci, fill = '#924900', alpha = 0.4) +
+  geom_line(aes(Max_Depth_cm, mu), prediction_depth, col = 'black', lwd = 1) +
+  scale_y_log10(labels = scales::label_number()) +
+  scale_x_log10() +
+  ylab("MP Concentrations (items/kg)") +
+  xlab("Soil Depth (cm)") +
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=15, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=15, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=15, family = "sans"),
+        axis.text.x  = element_text(size=15, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        legend.key.size = (unit(0.5, "cm")),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.5,0.2,0.2), "cm")) 
+  
+ggsave("pred_depth.png", width = 8, height = 6,
+       dpi = 600, units = "in") 
+
+
+
+# HFI Predictions ---------------------------------------------------------
+
+# Make a new dataframe
+newdf_HFI <- data.frame(HFI = seq(0, 1, by = 0.001),
+                          Max_Depth_cm = 1,
+                          Elevation_km = 10,
+                          Study = 'new study')
+
+prediction_HFI <- newdf_HFI
+prediction_HFI$mu <- predict(model,
+                               newdata = newdf_HFI,
+                               se.fit = FALSE,
+                               type = 'response')
+
+# Plot the predicted HFI
+ggplot() + 
+  geom_point(aes(HFI, Items_kg), MPdf) +
+  geom_line(aes(HFI, mu), prediction_HFI, col = 'red', lwd = 1) +
+  scale_y_log10() 
+
+# Create credible intervals
+fit_HFI <- predict(model, newdata = prediction_HFI, se.fit = TRUE,
+                     type = 'link')
+mu_HFI <- exp(fit_HFI$fit)
+lower_95_HFI <- exp(fit_HFI$fit - 1.96 * fit_HFI$se.fit)
+upper_95_HFI <- exp(fit_HFI$fit + 1.96 * fit_HFI$se.fit)
+
+# Combine dataframes
+prediction_HFI_ci <- data.frame(prediction_HFI,
+                                  fit_HFI,
+                                  mu = mu_HFI,
+                                  lower_95 = lower_95_HFI,
+                                  upper_95 = upper_95_HFI)
+
+# Plot the HFI prediction
+#pred_HFI <-
+  ggplot() +
+  geom_point(aes(HFI, Items_kg), MPdf, col = "#924900") +
+  geom_ribbon(aes(HFI, ymin = lower_95, ymax = upper_95), prediction_HFI_ci, fill = "#924900", alpha = 0.4) +
+  geom_line(aes(HFI, mu), prediction_HFI, col = 'black', lwd = 1) +
+  scale_y_log10(labels = scales::label_number()) +
+  ylab("MP Concentrations (items/kg)") +
+  xlab("HFI") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=15, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=15, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=15, family = "sans"),
+        axis.text.x  = element_text(size=15, family = "sans"), 
+        legend.position = "none",
+        legend.key.size = (unit(0.5, "cm")),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ 
+ggsave("pred_HFI.png", width = 8, height = 6,
+       dpi = 600, units = "in")
+
+
+# Elevation Predictions ---------------------------------------------------
+
+# Make a new dataframe
+newdf_elev <- data.frame(Elevation_km = seq(0, 2473, by = 1),
+                        HFI = 1,
+                        Max_Depth_cm = 10,
+                        Study = 'new study')
+
+prediction_elev <- newdf_elev
+prediction_elev$mu <- predict(model,
+                             newdata = newdf_elev,
+                             se.fit = FALSE,
+                             type = 'response')
+
+# Plot the predicted elevation
+ggplot() + 
+  geom_point(aes(Elevation_km, Items_kg), MPdf) +
+  geom_line(aes(Elevation_km, mu), prediction_elev, col = 'red', lwd = 1) +
+  scale_y_log10() 
+
+# Create credible intervals
+fit_elev <- predict(model, newdata = prediction_elev, se.fit = TRUE,
+                   type = 'link')
+mu_elev <- exp(fit_elev$fit)
+lower_95_elev <- exp(fit_elev$fit - 1.96 * fit_elev$se.fit)
+upper_95_elev <- exp(fit_elev$fit + 1.96 * fit_elev$se.fit)
+
+# Combine dataframes
+prediction_elev_ci <- data.frame(prediction_elev,
+                                fit_elev,
+                                mu = mu_elev,
+                                lower_95 = lower_95_elev,
+                                upper_95 = upper_95_elev)
+
+# Plot the elevation prediction
+#pred_elev <-
+  ggplot() +
+  geom_point(aes(Elevation_km, Items_kg), MPdf, col = "#924900") +
+  geom_ribbon(aes(Elevation_km, ymin = lower_95, ymax = upper_95), prediction_elev_ci, fill = "#924900", alpha = 0.4) +
+  geom_line(aes(Elevation_km, mu), prediction_elev, col = 'black', lwd = 1) +
+  scale_y_log10(labels = scales::label_number()) +
+  scale_x_log10() +
+  ylab("MP Concentrations (items/kg)") +
+  xlab("Elevation (m)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=15, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=15, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=15, family = "sans"),
+        axis.text.x  = element_text(size=15, family = "sans"), 
+        legend.position = "none",
+        legend.key.size = (unit(0.5, "cm")),
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+
+ggsave("pred_elev.png", width = 8, height = 6,
+         dpi = 600, units = "in")
+
+
+
+trends2 <-
+  grid.arrange(pred_HFI,pred_depth,
+               ncol = 2,
+               nrow = 1)
+
+trends22 <-
+  grid.arrange(trends2, pred_elev, 
+               ncol=1,
+               nrow=2,
+               heights = c(8,5.5),
+               widths = 6)
+
+ggsave("combined_proj_plot.png", plot = trends22, width = 8, height = 6,
+       dpi = 600, units = "in")
+
+
+
+
+
+
+
+# Another way to code the above:
+
+# pred_elev_f_ci <-
+#   bind_cols(newd_elev_f, #could also use cbind() here  
+#             as.data.frame(predict(FIT.2.1, newdata = pred_elev_f, se.fit = TRUE, type = 'link')))
+# 
+# 
+# pred_elev_f_ci <- mutate(pred_elev_f_ci, #mutate() is using dplyr 
+#                          mu = exp(fit), 
+#                          lower_95 = exp(fit - 1.96 * se.fit),
+#                          upper_95 = exp(fit + 1.96 * se.fit))
+
+
+
+#--------------------------------------------------------------------------
+# Observed vs. Predicted Plot   
+#--------------------------------------------------------------------------   
+
+# Using a tweedie distribution so it is normal for variance to look 
+#heteroskedastic (variance scales with the mean)
+
+ggplot(MPdf, aes(Items_kg, Predictions)) +
+  geom_abline(intercept = 0, slope = 1, color = 'red') +
+  geom_point() 
+
---
+++ Scripts/6_regression-kriging_100000_res.R
@@ -0,0 +1,183 @@
+setwd("C:/Users/lmills96/OneDrive - UBC/MSc Thesis Info/Global Analysis/Global MP Distribution/Rasters")
+
+# Load required packages
+
+library(sp)
+library(terra)
+library(sf)
+library(gstat)
+library(tidyverse)
+
+#----------------------------------------------------------------------
+# Predictions from the GAM
+#----------------------------------------------------------------------
+
+message("Predicting global MP concentrations from the fitted model")
+
+
+
+# Convert SpatRaster into dataframe 
+newdf <- terra::as.data.frame(HFI, xy = TRUE, row.names = FALSE)
+
+# Extract elevation from elevation_global SpatRaster (Elevation_km)
+el_values <- terra::extract(Elevation_km, cbind(newdf$x, newdf$y))
+
+# Add elevation to the HFI data frame. This will change depending on the
+# resolution you use 
+newdf$Elevation_km <- el_values$wc2.1_10m_elev
+
+#Define the depth we want to predict for (here just surface level)
+newdf$Max_Depth_cm <- as.integer(0)
+
+#Set a "study" Only needed for predicting
+newdf$Study <- 'prediction'
+
+# Rename the HFI column to math the model
+names(newdf)[3] <- "HFI"
+head(newdf)
+
+# Predict MP concentrations using the fitted model
+# newdf$mu <- predict(model,
+#                     newdata = newdf,
+#                     type = 'link',
+#                     se.fit = FALSE,
+#                     exclude = "Study")
+ 
+# save(newdf, file = "MP_Prediction_newdf_10res.Rda")
+
+# Create a raster from the predictions using entire data set
+MP_prediction_model <- rast(newdf[c("x", "y", "mu")], type="xyz", crs = crs_wintri)
+
+# The prediction here will be a data frame which will have to be 
+# converted back to a SpatRaster.
+mp_pred_model_pic <- plot(MP_prediction_model)
+
+dev.copy(png, filename = "mp_pred_model_pic.png")
+dev.off()
+
+
+
+#----------------------------------------------------------------------
+# Kriging the residuals
+#----------------------------------------------------------------------
+
+# Import the elevation raster 
+test <- Elevation_km
+
+# Import the residuals
+
+# Averaging all residuals that have the same coordinates so that there are only 
+# one value at each pair of coordinates.
+MPdf$residuals <- MPdf$Items_kg - predict(model, type = "response")
+data <- aggregate(residuals ~ x + y, data = MPdf, mean)
+
+#Convert to an sf object
+vg.data <- st_as_sf(vect(data[,c("x", "y", "residuals"),],
+                         geom=c("x", "y"),
+                         crs="ESRI:53018"))
+
+#Plot to make sure it looks OK
+plot(test)
+points(vg.data)
+
+
+# Empirical variogram for the MP model residuals
+mp.vg <- variogram(residuals ~ 1, data  = vg.data)
+plot(mp.vg, ylim = c(0, 9e9))
+
+
+# Note: 'Fit' model --- this is the result of fitting a theoretical variogram 
+# model to the empirical variogram, providing estimates for model parameters 
+# that characterize the spatial structure of the residual
+
+# B/c we're doing a variogram of the residuals, it already does this correction 
+# (moving the y to 0 - by definition the mean of my residuals is mean = 0) -> 
+# if the mean wasn't = 0 then my predictions are off by a certain amount so 
+# E(e) = E(y-mu) = 0
+mp.vg.fit <- fit.variogram(mp.vg, vgm("Exp", nugget = TRUE)) 
+
+
+# Plot variogram 
+
+plot(mp.vg, mp.vg.fit)
+# plot(mp.vg, mp.vg.fit,
+#      xlab = expression(bold("Lag Distance (m)")),
+#      ylab = expression(bold("Semivariance")))
+  
+
+# Fit variogram with different models and parameters
+mp.vg.fit.exp <- fit.variogram(mp.vg, vgm("Exp", psill = 1, range = 100, nugget = 0.1))
+mp.vg.fit.sph <- fit.variogram(mp.vg, vgm("Sph", psill = 1, range = 100, nugget = 0.1))
+mp.vg.fit.gau <- fit.variogram(mp.vg, vgm("Gau", psill = 1, range = 100, nugget = 0.1))
+
+# Print the fitted variogram models
+plot(mp.vg, mp.vg.fit.exp)
+plot(mp.vg, mp.vg.fit.sph)
+plot(mp.vg, mp.vg.fit.gau)
+
+
+mp.vg.test <- autofitVariogram(residuals ~ 1, input_data = vg.data, model = "Exp", verbose = TRUE)
+plot(mp.vg.fit.test, cutoff = 10500)
+mp.vg.fit.test <- fit.variogram(mp.vg.test, vgm("Exp", psill = 1.5, range = 1500, nugget = 0.0001)) 
+plot(mp.vg.test, mp.vg.fit.test)
+
+#need to save ggplot of variogram
+
+
+
+# Global Kriging ------------------------------------------------------
+
+# This is currently at 100,000 resolution (low)
+
+
+# Step 1: Define a grid based on the bounding box of the elevation raster
+# Currently using tidy verse, but better to use base R
+# grd_100_sf_100000 <- test %>% 
+#   st_bbox() %>% 
+#   st_as_sfc() %>% 
+#   st_make_grid(
+#     cellsize = c(100000, 100000) # pixel size (Made it large here for fast computation time, should be as small as computatioanlly feasible)
+#   )
+
+#save(grd_100_sf_100000, file = "grd_100_sf_100000.Rda")
+
+
+# Ordinary Kriging of the residuals
+# mp_predictions_krige_100000 <- krige(
+#   residuals ~ 1,
+#   vg.data,
+#   grd_100_sf_100000,
+#   model = mp.vg.fit)
+
+# save(mp_predictions_krige_100000, file = "mp_predictions_krige_100000.Rda")
+
+
+# Convert prediction to SpatRaster (using the tidyterra package for convenience, 
+# not because it's the only way to do this)
+tester <- tidyterra::as_spatraster(mp_predictions_krige_100000, crs = "ESRI:53018")
+tester <- tester["var1.pred"]
+
+tester_cropped <- tester %>%
+  terra::crop(world_wintri, mask = T)
+
+# Match all resolution based HFI resolution
+var1_resampled <- terra::resample(tester_cropped, MP_prediction_model, method = "bilinear")
+
+
+
+#----------------------------------------------------------------------
+# Combined regression-kriging predictions
+#----------------------------------------------------------------------
+
+# Get the regression kriging predictions by summing the two
+MP_prediction <- MP_prediction_model + var1_resampled
+
+# Convert back to the correct scale
+MP_prediction <- exp(MP_prediction) 
+plot(MP_prediction)
+# writeRaster(MP_prediction, filename = "C:/Users/lmills96/OneDrive - UBC/MSc Thesis Info/Global Analysis/Global MP Distribution/Rasters/kriging_raster.tif", overwrite = TRUE)
+
+
+# Putting regression-kriging back on the response scale 
+MP_prediction_model_response_scale <- exp(MP_prediction_model)
+plot(MP_prediction_model_response_scale)
