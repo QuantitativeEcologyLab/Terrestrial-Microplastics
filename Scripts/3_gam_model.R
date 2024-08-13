50c547be8ac6a61ea0c5253ab0b13bcc7f730dbf
MILLS <lauren.mills@ubc.ca>
2024-08-13 16:06:21 -0700

    updated model with updated dataset

--- Scripts/3_gam_model.R
+++ Scripts/3_gam_model.R
@@ -3,9 +3,9 @@ library(mgcv)
 
 model <- gam(Items_kg ~
                # global terms
-               s(HFI, k = 12, bs = "tp") + 
+               s(HFI, k = 8, bs = "ad") + #bs="tp" is the default so I don't need to specify?
                s(Max_Depth_cm, k = 5) +
-               s(Elevation_km, k = 10) +
+               s(Elevation_km, k = 6) + #EDIT KNOTS - 6 AT THE ABSOLUTE MOST
                #ti(Elevation_km, HFI, k = 5, bs = "tp"),# + #don't need study in here b/c interaction term
                #ti(Max_Depth_cm, HFI, k = 5, bs = "tp") +
                # study-level terms
@@ -14,6 +14,8 @@ model <- gam(Items_kg ~
              family = tw(link = 'log'),
              data = MPdf,
              method = "REML")
+
+
 
 plot(model, pages = 1, scheme = 3, scale = 0)
 
@@ -36,3 +38,5 @@ total_dup <- sum(duplicated(MPdf[, c("x", "y")]))
 total_dup
 
 
+
+#Histogram could be density and overlay histogram w/ frequency on it (for histogram script)
