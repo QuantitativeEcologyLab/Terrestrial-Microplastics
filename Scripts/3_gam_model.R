ae101a05ea5b62be8e8589bdb43d3eabec9603e0
MILLS <lauren.mills@ubc.ca>
2024-08-13 21:09:51 -0700

    revised gam

--- Scripts/3_gam_model.R
+++ Scripts/3_gam_model.R
@@ -3,9 +3,9 @@ library(mgcv)
 
 model <- gam(Items_kg ~
                # global terms
-               s(HFI, k = 8, bs = "ad") + #bs="tp" is the default so I don't need to specify?
-               s(Max_Depth_cm, k = 5) +
-               s(Elevation_km, k = 6) + #EDIT KNOTS - 6 AT THE ABSOLUTE MOST
+               s(HFI, k = 10, bs = "ad") + #bs="tp" is the default so I don't need to specify?
+               s(sqrt(Max_Depth_cm), k = 5) +
+               s(sqrt(Elevation_km), k = 6) + #EDIT KNOTS - 6 AT THE ABSOLUTE MOST
                #ti(Elevation_km, HFI, k = 5, bs = "tp"),# + #don't need study in here b/c interaction term
                #ti(Max_Depth_cm, HFI, k = 5, bs = "tp") +
                # study-level terms
@@ -16,7 +16,7 @@ model <- gam(Items_kg ~
              method = "REML")
 
 
-
+par(mar = c(3, 3, 3, 1) + 0.1)
 plot(model, pages = 1, scheme = 3, scale = 0)
 
 MPdf$residuals <- MPdf$Items_kg - predict(model, type = "response")
@@ -32,11 +32,7 @@ summary(model)
 #partial effect on the y 
 
 
-
 # Averaging residuals from duplicated coordinates
 total_dup <- sum(duplicated(MPdf[, c("x", "y")]))
 total_dup
 
-
-
-#Histogram could be density and overlay histogram w/ frequency on it (for histogram script)
