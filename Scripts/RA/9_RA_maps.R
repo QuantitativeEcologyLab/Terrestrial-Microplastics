629a6dd892ecb9245cf9f33c21d333969cb89841
MILLS <lauren.mills@ubc.ca>
2024-09-26 18:24:56 -0700

    updated script

--- Scripts/RA/9_RA_maps.R
+++ Scripts/RA/9_RA_maps.R
@@ -3,8 +3,8 @@ library(geodata)
 library(terra)
 library(sf)
 
-bc <- gadm(country="Canada", level = 1, tempdir())
-bc <- bc[bc$NAME_1=="British Columbia", ]
+bc <- gadm(country="Canada", level = 2, tempdir())
+bc <- bc[bc$NAME_2=="Thompson-Nicola", ]
 
 plot(bc)
 
@@ -38,11 +38,13 @@ RA_Coords <- st_as_sf(RA_Coords)
 # geometries after reprojection)
 RA_Coords <- cbind(RA_Coords, st_coordinates(RA_Coords))
 
+crs(bc) == crs(RA_Coords)
+
 # Note: don't use RA_Coords$x <- geom(RA_Coords)[,1] here for instance b/c it is
 # extracting from a SpatVector 
 
 
-plot_bc <- 
+#plot_bc <- 
   ggplot() +
   geom_sf(data=bc, fill = "#6a994e", color="black", size=.15, show.legend = FALSE) +
   geom_point(data = RA_Coords, aes(x = X, y = Y), color = "#924900", size = 0.6) +
@@ -53,3 +55,54 @@ plot_bc <-
   no_axis
 
 plot_bc
+
+
+
+
+
+
+# ----------------- Adding RA coordinates to map ------------------
+
+library(leaflet)
+library(sf)
+
+RA_map <- 
+  leaflet(RA_Coords) %>%
+  addTiles() %>%
+  setView(lng = -120.530275, lat = 50.457125, zoom = 9) %>%
+  addCircleMarkers(~X, ~Y,
+                   radius = 5,
+                   color = "black",
+                   fillColor = "magenta",
+                   fillOpacity = .5,
+                   stroke = TRUE,
+                   weight = 1)
+
+# Saving as a static 
+library(mapview)
+webshot::install_phantomjs()
+mapshot(RA_map, file= "figures/RA_map.png")
+
+
+# HSN and HSR only
+
+Hamilton_Stipa <- RA_Coords[-c(1:29,32,34,36:47,49,52),]
+
+
+
+HS_map <- 
+  leaflet(Hamilton_Stipa) %>%
+  addTiles() %>%
+  setView(lng = -120.448376, lat = 50.073852, zoom = 15) %>%
+  addCircleMarkers(~X, ~Y,
+                   radius = 5,
+                   color = "black",
+                   fillColor = "magenta",
+                   fillOpacity = .5,
+                   stroke = TRUE,
+                   weight = 1)
+
+mapshot(HS_map, file= "figures/HS_map.png")
+
+
+
