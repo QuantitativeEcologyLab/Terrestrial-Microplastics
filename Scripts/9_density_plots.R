114657b596f51eb028bd0147d7af30013b2e2b94
MILLS <lauren.mills@ubc.ca>
2024-09-29 11:20:50 -0700

    updated script

--- Scripts/9_density_plots.R
+++ Scripts/9_density_plots.R
@@ -1,3 +1,5 @@
+library(gridExtra)
+
 
 # --------------------------------------------------------
   #HFI density plot
@@ -21,18 +23,18 @@ HFI_density <-
   theme_minimal() +
   xlab("HFI") +
   ylab ("Density") +
-  scale_y_sqrt() +
+  #scale_y_sqrt() +
   scale_fill_manual(values = c("#515151", "red"),
-                    labels = c("Global HFI", "Samples taken across HFI gradient")) +
+                    labels = c("Global HFI", "Samples taken")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.x = element_text(size=10, family = "sans", face = "bold"),
         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
         axis.text.x = element_text(size=9, family = "sans", face = "bold", colour = "black"),
-        axis.text.y  = element_text(size=9, family = "sans"),
+        axis.text.y  = element_text(size=9, family = "sans", face = "bold"),
         strip.text.x = element_text(size=9, family = "sans", face = "bold"),
         plot.title = element_text(hjust = -0.05, size = 8, family = "sans", face = "bold"),
-        legend.position = c(0.7, 0.85),
+        legend.position = c(0.4, 0.85),
         legend.title = element_blank(),
         legend.text = element_text(size=9, family = "sans", face = "bold"),
         legend.background = element_rect(fill = "transparent"),
@@ -40,10 +42,11 @@ HFI_density <-
         legend.spacing.y = unit(0.1, 'cm'),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
-        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) 
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
+    ggtitle("A)")
 
 ggsave("HFI_density.png", plot = HFI_density, width = 6,
-       height = 4, units = "in", dpi = 300)
+       height = 4, units = "in", dpi = 600)
 
 
 # --------------------------------------------------------
@@ -70,19 +73,21 @@ elev_density <-
   ggplot(combined_elev, aes(x = Elevation_km, fill = dataset)) +
   geom_histogram(aes(y = ..density..), alpha = 0.5, position = "identity", bins = 30) +
   theme_minimal() +
+    labs(y = NULL) +
   xlab("Elevation") +
-  ylab ("Density") +
+  #ylab ("Density") +
+  #scale_y_sqrt() +
   scale_fill_manual(values = c("#515151", "red"),
-                    labels = c("Global elevation", "Samples taken across elevation gradient")) +
+                    labels = c("Global elevation", "Samples taken")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.x = element_text(size=10, family = "sans", face = "bold"),
         axis.title.y = element_text(size=10, family = "sans", face = "bold"),
         axis.text.x = element_text(size=9, family = "sans", face = "bold", colour = "black"),
-        axis.text.y  = element_text(size=9, family = "sans"),
+        axis.text.y  = element_text(size=9, family = "sans",  face = "bold"),
         strip.text.x = element_text(size=9, family = "sans", face = "bold"),
         plot.title = element_text(hjust = -0.05, size = 8, family = "sans", face = "bold"),
-        legend.position = c(0.7, 0.85),
+        legend.position = c(0.4, 0.85),
         legend.title = element_blank(),
         legend.text = element_text(size=9, family = "sans", face = "bold"),
         legend.background = element_rect(fill = "transparent"),
@@ -90,7 +95,8 @@ elev_density <-
         legend.spacing.y = unit(0.1, 'cm'),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
-        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) 
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
+    ggtitle("B)")
 
 ggsave("elev_density.png", plot = elev_density, width = 6,
        height = 4, units = "in", dpi = 300)
@@ -102,26 +108,46 @@ ggsave("elev_density.png", plot = elev_density, width = 6,
 
 # No density plot for global depth 
 
-#depth_density <- 
-ggplot(MPdf, aes(x = Max_Depth_cm)) +
+depth_density <-
+  ggplot(MPdf, aes(x = Max_Depth_cm, fill = "red")) +
   geom_histogram(aes(y = ..density..), alpha = 0.5, position = "identity", bins = 30) +
-  labs(title = "",
-       x = "Soil Depth (cm)",
-       y = "Frequency") +
-  theme_bw()+
+  theme_minimal() +
+  labs(y = NULL) +
+  xlab("Soil Depth (cm)") +
+  #ylab ("Density") +
+  #scale_y_sqrt() +
+  scale_fill_manual(values = c("red"),
+                    labels = c("Samples taken")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.title.y = element_text(size=15, family = "sans", face = "bold"),
-        axis.title.x = element_text(size=15, family = "sans", face = "bold"),
-        axis.text.y = element_text(size=15, family = "sans"),
-        axis.text.x  = element_text(size=15, family = "sans"),
-        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
-        legend.position = "right",
-        legend.key.size = (unit(0.5, "cm")),
+        axis.title.x = element_text(size=10, family = "sans", face = "bold"),
+        axis.title.y = element_text(size=10, family = "sans", face = "bold"),
+        axis.text.x = element_text(size=9, family = "sans", face = "bold", colour = "black"),
+        axis.text.y  = element_text(size=9, family = "sans",  face = "bold"),
+        strip.text.x = element_text(size=9, family = "sans", face = "bold"),
+        plot.title = element_text(hjust = -0.05, size = 8, family = "sans", face = "bold"),
+        legend.position = c(0.4, 0.85),
+        legend.title = element_blank(),
+        legend.text = element_text(size=9, family = "sans", face = "bold"),
+        legend.background = element_rect(fill = "transparent"),
+        legend.key.size = unit(0.2, 'cm'),
+        legend.spacing.y = unit(0.1, 'cm'),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
-        plot.margin = unit(c(0.2,0.5,0.2,0.2), "cm")) 
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm")) +
+  ggtitle("C)")
 
 ggsave("depth_density.png", plot = depth_density, width = 6,
        height = 4, units = "in", dpi = 300)
 
+
+
+
+density_plots <-
+  grid.arrange(HFI_density,elev_density,depth_density,
+               ncol = 3,
+               nrow = 1,
+               widths = c(6,6,6))
+
+ggsave("density_plots.png", plot = density_plots, width = 10,
+       height = 4, units = "in", dpi = 600)
