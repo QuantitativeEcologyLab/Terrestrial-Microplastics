8d165665692b8679a0f1b6aff2ffaf877cb2f54c
MILLS <lauren.mills@ubc.ca>
2025-07-29 22:39:04 -0700

    fixed script and raster values

--- Scripts/02c_predicting_raster_NA_values.R
+++ Scripts/02c_predicting_raster_NA_values.R
@@ -9,17 +9,17 @@ MPdf_NAs <- MPdf[!complete.cases(MPdf),]
 
 # Get model based off dataset with no NAs (for predictions)
 model_HFI <- 
-              gam(HFI ~
-              # global terms
-              s(Items_kg) + 
-              s(sqrt(max_depth_cm), k = 5) +
-              s(elevation_m, k = 6) + 
-              # study-level terms
-              s(study, bs = 're'), #random int - it doesn't really have random slopes 
-            family = tw(link = 'log'),
-            data = MPdf_subset,
-            method = "REML",
-            na.action = na.fail)
+  gam(HFI ~
+      # global terms
+      #s(Items_kg) + 
+      s(sqrt(max_depth_cm), k = 5) +
+      s(elevation_m, k = 6) + 
+      # study-level terms
+      s(study, bs = 're'), #random int - it doesn't really have random slopes 
+    family = tw(link = 'log'),
+    data = MPdf_subset,
+    method = "REML",
+    na.action = na.fail)
 
 # Create dataset with coordinates where there were NAs for each raster separately
 MPdf_NAs_HFI <- MPdf[!complete.cases(MPdf$HFI),]
@@ -42,7 +42,7 @@ MPdf$HFI[na_rows] <- MPdf_NAs_HFI$HFI
 model_soil <- 
   gam(soil_type ~
         # global terms
-        s(Items_kg) + 
+        #s(Items_kg) + 
         s(sqrt(max_depth_cm), k = 5) +
         s(elevation_m, k = 6) + 
         s(HFI, k = 10, bs = "ad") + 
@@ -58,7 +58,7 @@ MPdf_NAs_soil <- MPdf[!complete.cases(MPdf$soil_type),]
 
 # Note: need at least elevation or soil type to estimate HFI - if all 3 rasters
 # were NA, you'd need to estimate one prior to predicting HFI
-MPdf_NAs_soil$soil_pred <- predict(model_HFI,
+MPdf_NAs_soil$soil_pred <- predict(model_soil,
                                  newdata = MPdf_NAs_soil)
 
 # Add predicted soil values back into MPdf
@@ -71,8 +71,6 @@ MPdf$soil_type[na_rows] <- MPdf_NAs_soil$soil_type
 
 # Save final dataset
 write.csv(MPdf, file = "./Data/MPdf.csv", row.names = FALSE)
-
-
 
 # Note: this prediction code differs from 05_MP_predictions.R as that script
 # is using average values to predict overall trends. Here we are interested
