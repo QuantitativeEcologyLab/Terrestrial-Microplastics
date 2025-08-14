bf7a1eafe90174de52461a37579d71d81c391b10
MILLS <lauren.mills@ubc.ca>
2025-08-14 15:02:23 -0700

    cleaned up script

--- Scripts/06c_regression-Kriging.R
+++ Scripts/06c_regression-Kriging.R
@@ -30,14 +30,14 @@ newdf <- readRDS("./Data/newdf.RDS")
 # Load the GAM prediction raster
 MP_prediction_model <- rast("./Rasters/GAM_prediction_model.tif")
 
+# Load bounding box of case study (new_EXT)
+new_EXT <- readRDS("./Data/new_EXT.RDS")
+
 # Load Kriged residuals
-# OK_100 <- loadRDS("./Data/OK_100.RDS")
+# OK_500 <- loadRDS("./Data/OK_500.RDS")
 
 # Load var1 raster
 # var1_resampled <- rast("./Rasters/var1_resampled.tif")
-
-
-
 
 #----------------------------------------------------------------------
 # Predictions from the GAM
@@ -60,7 +60,7 @@ writeRaster(MP_prediction_model,
 #----------------------------------------------------------------------
 
 # Import the residuals 
-MPdf$residuals <- residuals(model, type = "working") 
+#MPdf$residuals <- residuals(model, type = "working") 
 
 #Subset data
 MPdf_study <- MPdf[MPdf$study == "61",]
@@ -84,7 +84,6 @@ plot(mp.vg)
 mp.vg.fit <- fit.variogram(mp.vg, vgm("Sph", nugget = TRUE))
 plot(mp.vg, mp.vg.fit) 
 
-
 # Convert HFI to an sf crs object
 crs_sf <- sf::st_crs(terra::crs(HFI))
 
@@ -94,34 +93,54 @@ China_bbox_sf <- st_as_sfc(st_bbox(new_EXT))
 # Assign the same CRS to China_bbox_sf as other sf objects
 sf::st_crs(China_bbox_sf) <- crs_sf
 
+
+print(mp.vg.fit) 
+# Note: 
+  # 1) vg range = 12,527 meters, Kriging is based of bounding box of 306,864 m^2 
+  # 2) total sill = nugget + partial sill = 2.493077
+      # the portion of variance explained by spatial structure = partial/total
+      # = 0.4855 / 48.6%
+      # 51.4% is the nugget (variation at very small spatial scales - i.e., 
+      # measurement error, microscale randomness, etc.)
+  # 3) if half the variation is from nugget, predicting at very fine scales may
+      # mean the unaccounted-for variation by the nugget makes predictions      
+      # less accurate
+  # 4) using a 500m x 500m grid size for predictions
+
+
 # Create a grid of square polygons over the bbox (at 100 res)
-grid_10_sf <- st_make_grid(China_bbox_sf, cellsize = c(10, 10))
+grid_500_sf <- st_make_grid(China_bbox_sf, cellsize = c(500, 500))
 
 # Ordinary Kriging of the residuals (will be an sf data frame)
-OK_10 <- 
+OK_500 <- 
   krige(residuals ~ 1, # kriging the residuals = the formula
         vg.data, # provides coords & residuals from sample locations = the locations
-        grid_10_sf, # gives the locations to predict the residuals at = newdata
+        grid_500_sf, # gives the locations to predict the residuals at = newdata
         model = mp.vg.fit) # gives theoretical vg model describing how residuals vary with distance = model
-saveRDS(OK_10, file = "/home/lmills96/Documents/GDMP/Rasters/OK_10.tif")
+saveRDS(OK_500, file = "/home/lmills96/Documents/GDMP/Rasters/OK_500.tif")
 
 
 # Convert the Kriging back to SpatRaster 
   # Note: using the tidyterra package for convenience, 
   # not because it's the only way to do this
-tester <- tidyterra::as_spatraster(OK_10, crs = crs(HFI))
-  # Note: tester["var1.pred] is returning a subset of tester with ONLY the
-  # column of "var1.pred" vs. tester$var1.pred would return a vector
-tester <- tester["var1.pred"]
+Kriging_raster <- tidyterra::as_spatraster(OK_500, crs = crs(HFI))
+ 
+# Extract the predicted values at each location (the interpolated residuals) 
+  # Note: Kriging_raster["var1.pred] is returning a subset of Kriging_raster 
+    # with ONLY the column of "var1.pred" vs. Kriging_raster$var1.pred would 
+    # return a vector
+  # Note: Kriging_raster["var1.var"] is the Kriging variance (uncertainty of the 
+    # prediction at each location) so we do not want this
+Kriging_raster <- Kriging_raster["var1.pred"]
 
-plot(tester)
+plot(Kriging_raster)
 
 # Crop 
-tester_cropped <- tester %>%
+Kriging_raster_cropped <- Kriging_raster %>%
   terra::crop(HFI_crop, mask = T)
 
 # Match all resolution, extent, and CRS of Kriged residuals to the GAM raster
-var1_resampled <- terra::resample(tester_cropped, MP_prediction_model, 
+var1_resampled <- terra::resample(Kriging_raster_cropped, MP_prediction_model, 
                                   method = "bilinear")
 
 # Save the Kriging predictions 
@@ -143,33 +162,3 @@ RK <- MP_prediction_model + var1_resampled
 RK <- exp(RK) 
 plot(RK)
 writeRaster(RK, filename = "./Rasters/RK_raster.tif", overwrite = TRUE)
-
-
-
-
-# -------------------------------------
-
-# Global Kriging ------------------------------------------------------
-# This is currently at 100,000 resolution (low)
-
-# Get bounding box of elevation raster
-elev_bbox <- st_bbox(Elevation_m)
-
-# Convert elevation bounding box to an sf polygon
-elev_sf <- st_as_sfc(elev_bbox)
-
-# Create a grid of square polygons over the bounding box (at 100000 res)
-grid_100000 <- st_make_grid(elev_sf, cellsize = c(100000, 100000))
-# note: pixel size is large for fast computation time
-
-grid_100000_sf <- sf::st_sf(grid_100000) # as sf object rather than sfc?
-#saveRDS(grid_100000, file = "grid_100000.RDS")
-test_crs <- readRDS("/home/lmills96/Downloads/kriging_100000.RDS")
-# Ordinary Kriging of the residuals
-kriging_100000 <-
-  krige(residuals ~ 1, # kriging the residuals = the formula
-        vg.data, # provides coordinates & residuals from sample locations = the locations
-        grid_100000_sf, # gives the locations to predict the residuals at = newdata
-        model = mp.vg.fit) # gives theoretical vg model describing how residuals vary with distance = model
-# note: if you were to do universal kriging, formula would be z ~ x+y rather than z ~ 1
-#saveRDS(kriging_100000, file = "./Data/kriging_100000.RDS")
